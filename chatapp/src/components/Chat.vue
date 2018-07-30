f5a0ea54d18e0fafc6415a2ebb0d05e1f110a29f
prateek951 <prince.cenation@gmail.com>
2018-07-30 20:50:20 +0530

    customised scrollbar

--- chatapp/src/components/Chat.vue
+++ chatapp/src/components/Chat.vue
@@ -76,4 +76,17 @@ export default {
   display: block;
   font-size: 0.8em;
 }
+.messages {
+    max-height: 300px;
+    overflow: auto;
+}
+.messages::-webkit-scrollbar {
+    width: 3px;
+}
+.messages::-webkit-scrollbar-track {
+    background: #ddd;
+}
+.messages::-webkit-scrollbar-thumb {
+    background: #aaa;
+}
 </style>
