093c228c26eba28f982c6385a90014c04ae3b538
prateek951 <prince.cenation@gmail.com>
2018-07-30 19:02:07 +0530

    working on chat component!

--- chatapp/src/components/Chat.vue
+++ chatapp/src/components/Chat.vue
@@ -1,6 +1,20 @@
 <template>
     <div class="chat container">
-        <h2>Chat {{this.name}}</h2>
+        <h2 class="center green-text">WeChat</h2>
+        <div class="card">
+            <div class="card-content">
+                <ul class="messages">
+                    <li>
+                        <span class="teal-text">Name</span>
+                        <span class="grey-text text-darken-3">message</span>
+                        <span class="grey-text time">time</span>
+                    </li>
+                </ul>
+            </div>
+            <div class="card-action">
+                <input type="text">
+            </div>
+        </div>
 
     </div>
 </template>
@@ -9,7 +23,7 @@
 <script>
 export default {
   name: "Chat",
-  props: ['name'],
+  props: ["name"],
   data() {
     return {};
   },
@@ -17,3 +31,18 @@ export default {
   computed: {}
 };
 </script>
+
+
+<style scoped>
+.chat h2 {
+    font-size: 2.6em;
+    margin-bottom: 40px;
+}
+.chat span {
+    font-size: 1.4em;
+}
+.chat .time {
+    display: block;
+    font-size: 1.2em;
+}
+</style>
